01890cb806c53f409ebc9bcb0b8b858a6640cf4f
oss-sp <90921202+oss-sp@users.noreply.github.com>
2021-10-24 18:11:34 +0900

    delete empty lines

--- CalcTest.java
+++ CalcTest.java
@@ -2,13 +2,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalcTest {
-
 	@Test
 	public void testAdd() {
 		Calc c = new Calc();
 		assertEquals(30, c.add(10, 20));
 	}
-	
 	@Test
 	public void testSub() {
 		Calc c = new Calc();
