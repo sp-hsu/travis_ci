051ddaa617b7fb1564082273d569ac78f6dbfba7
Seunghyun Park <sp@hansung.ac.kr>
2021-10-28 13:14:16 +0900

    func mul

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,8 +1,12 @@
+/* Calc.java */
 public class Calc {
 	public int add(int x, int y) {
 		return x+y;
 	}
 	public int sub(int x, int y) {
 		return x-y;
+	}
+	public int mul(int x, int y) {
+		return x*y;
 	}
 }
